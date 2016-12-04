cb32474d0705eb64f081b484a4b420cb56a06946
Marti_2203 <marti_2203@abv.bg>
2016-12-04 12:02:15 +0200

    Added Filter Button

---
+++ Book Downloader/Book Downloader/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Book_Downloader
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.ElementsDataView = new System.Windows.Forms.DataGridView();
+            this.FindButton = new System.Windows.Forms.Button();
+            this.PageTextBox = new System.Windows.Forms.TextBox();
+            this.PageLabel = new System.Windows.Forms.Label();
+            this.HTMLOutput = new System.Windows.Forms.TextBox();
+            this.FilterButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ElementsDataView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.3F);
+            this.NameLabel.Location = new System.Drawing.Point(9, 38);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(41, 15);
+            this.NameLabel.TabIndex = 1;
+            this.NameLabel.Text = "Name";
+            this.NameLabel.UseMnemonic = false;
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.NameTextBox.Location = new System.Drawing.Point(53, 38);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(113, 20);
+            this.NameTextBox.TabIndex = 2;
+            // 
+            // ElementsDataView
+            // 
+            this.ElementsDataView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ElementsDataView.Location = new System.Drawing.Point(53, 87);
+            this.ElementsDataView.Name = "ElementsDataView";
+            this.ElementsDataView.Size = new System.Drawing.Size(681, 486);
+            this.ElementsDataView.TabIndex = 3;
+            this.ElementsDataView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.ElementsDataView_CellContentClick);
+            // 
+            // FindButton
+            // 
+            this.FindButton.Location = new System.Drawing.Point(172, 48);
+            this.FindButton.Name = "FindButton";
+            this.FindButton.Size = new System.Drawing.Size(72, 26);
+            this.FindButton.TabIndex = 4;
+            this.FindButton.Text = "Find Books";
+            this.FindButton.UseVisualStyleBackColor = true;
+            this.FindButton.Click += new System.EventHandler(this.FindButton_Click);
+            // 
+            // PageTextBox
+            // 
+            this.PageTextBox.Location = new System.Drawing.Point(53, 64);
+            this.PageTextBox.Name = "PageTextBox";
+            this.PageTextBox.Size = new System.Drawing.Size(113, 20);
+            this.PageTextBox.TabIndex = 5;
+            // 
+            // PageLabel
+            // 
+            this.PageLabel.AutoSize = true;
+            this.PageLabel.Location = new System.Drawing.Point(13, 64);
+            this.PageLabel.Name = "PageLabel";
+            this.PageLabel.Size = new System.Drawing.Size(32, 13);
+            this.PageLabel.TabIndex = 6;
+            this.PageLabel.Text = "Page";
+            // 
+            // HTMLOutput
+            // 
+            this.HTMLOutput.Location = new System.Drawing.Point(740, 87);
+            this.HTMLOutput.Multiline = true;
+            this.HTMLOutput.Name = "HTMLOutput";
+            this.HTMLOutput.Size = new System.Drawing.Size(389, 486);
+            this.HTMLOutput.TabIndex = 7;
+            // 
+            // FilterButton
+            // 
+            this.FilterButton.Location = new System.Drawing.Point(277, 48);
+            this.FilterButton.Name = "FilterButton";
+            this.FilterButton.Size = new System.Drawing.Size(98, 29);
+            this.FilterButton.TabIndex = 8;
+            this.FilterButton.Text = "Filter Results";
+            this.FilterButton.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1141, 578);
+            this.Controls.Add(this.FilterButton);
+            this.Controls.Add(this.HTMLOutput);
+            this.Controls.Add(this.PageLabel);
+            this.Controls.Add(this.PageTextBox);
+            this.Controls.Add(this.FindButton);
+            this.Controls.Add(this.ElementsDataView);
+            this.Controls.Add(this.NameTextBox);
+            this.Controls.Add(this.NameLabel);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ElementsDataView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.DataGridView ElementsDataView;
+        private System.Windows.Forms.Button FindButton;
+        private System.Windows.Forms.TextBox PageTextBox;
+        private System.Windows.Forms.Label PageLabel;
+        private System.Windows.Forms.TextBox HTMLOutput;
+        private System.Windows.Forms.Button FilterButton;
+    }
+}
+
---
+++ Book Downloader/Book Downloader/Form1.cs
@@ -0,0 +1,127 @@
+﻿#define Release
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Web;
+using System.Net;
+using System.IO;
+using System.Threading;
+
+namespace Book_Downloader
+{
+    public partial class MainForm : Form
+    {
+        private const string _address = "http://gen.lib.rus.ec/search.php?&req={0}&phrase=1&view=simple&column=def&sort=def&sortmode=ASC&page={1}";
+        public MainForm()
+        {
+            InitializeComponent();
+            HTMLOutput.ScrollBars = ScrollBars.Both;
+            ElementsDataView.Columns.Add(new DataGridViewTextBoxColumn { Name = "Name" });
+            ElementsDataView.Columns.Add(new DataGridViewTextBoxColumn { Name = "Address" });
+            ElementsDataView.Columns.Add(new DataGridViewTextBoxColumn { Name = "Language" });
+            ElementsDataView.Columns.Add(new DataGridViewTextBoxColumn { Name = "Extension" });
+            FilterButton.Enabled = false;
+        }
+
+        private void FindButton_Click(object sender, EventArgs e)
+        {
+            FindButton.Enabled = false;
+            FilterButton.Enabled = false;
+            ElementsDataView.Enabled = false;
+            ElementsDataView.Rows.Clear();
+            ElementsDataView.Refresh();
+
+            new Thread(() =>
+            {
+
+                string code;
+                using (WebClient client = new WebClient())
+                {
+                    code = client.DownloadString(string.Format(_address, NameTextBox.Text, PageTextBox.Text == "" ? "1" : PageTextBox.Text));
+                }
+                HTMLOutput.Invoke(new MethodInvoker(() => HTMLOutput.Text = string.Format(_address + "\n", NameTextBox.Text, PageTextBox.Text == "" ? "1" : PageTextBox.Text)));
+                HTMLOutput.Invoke(new MethodInvoker(() => HTMLOutput.AppendText(code)));
+
+                string[] names = code
+                            .Split('\n')
+                            .Where(element => element.Contains("<td width="))
+                            .Select(element => element.Split('>')[2])
+                            .Select(element => element.Split('<')[0])
+                            .ToArray();
+
+                string[] namesAndExtensions = code
+                    .Split('\n')
+                    .Where(element => element.Contains("<td nowrap"))
+                    .Where((element, index) => index % 2 == 1)
+                    .Select(element => element.Split('<', '>')[2])
+                    .ToArray();
+
+                string[] addresses = code
+                    .Split('\n')
+                    .Where(element => element.Contains("http://libgen.io/ads.php?md5="))
+                    .Select(element => element.Split('\'')[1])
+                    .ToArray();
+
+                HTMLOutput.Invoke(new MethodInvoker(() => HTMLOutput.AppendText(namesAndExtensions.Length + " " + names.Length + " " + addresses.Length)));
+
+                for (int i = 0; i < addresses.Length; i++)
+                {
+                    try
+                    {
+                        ElementsDataView.Invoke(new MethodInvoker(() => ElementsDataView.Rows.Add(names[i], addresses[i])));
+                    }
+                    catch (IndexOutOfRangeException ior)
+                    {
+                        throw new IndexOutOfRangeException(ior.Message + " " + i + " " + addresses.Length + " " + names.Length);
+                    }
+                }
+                for (int j = 0, i = 0; j <= 24; j++, i += 2)
+                {
+                    ElementsDataView[2, j] = new DataGridViewTextBoxCell { Value = namesAndExtensions[i] };
+                    ElementsDataView[3, j] = new DataGridViewTextBoxCell { Value = namesAndExtensions[i + 1] };
+                }
+
+                ElementsDataView.Invoke(new MethodInvoker(() => ElementsDataView.AutoResizeColumns()));
+                ElementsDataView.Invoke(new MethodInvoker(() => ElementsDataView.AutoResizeRows()));
+                FilterButton.Invoke(new MethodInvoker(() => FilterButton.Enabled = true));
+            }).Start();
+
+            FindButton.Enabled = true;
+            ElementsDataView.Enabled = true;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ElementsDataView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 1)
+            {
+                using (WebClient client = new WebClient())
+                {
+                    ElementsDataView.Enabled = false;
+                    string code = client.DownloadString((string)ElementsDataView[e.ColumnIndex, e.RowIndex].Value);
+                    HTMLOutput.Text = code;
+                    string key = code
+                        .Split('\n')
+                        .Where(line => line.Contains("DOWNLOAD"))
+                        .First()
+                        .Split('>')[1]
+                        .Split('=')[3];
+                    HTMLOutput.Text = key.Remove(key.Length - 1);
+
+                    ElementsDataView.Enabled = true;
+                }
+            }
+        }
+    }
+}
