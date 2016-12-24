559392902a53358eb3693a4992b3ec70d37446b9
Marti_2203 <marti_2203@abv.bg>
2016-12-24 22:47:57 +0200

    Implemented Base Logger. Finally!

--- Book Downloader/Book Downloader/BaseLogger.cs
+++ Book Downloader/Book Downloader/BaseLogger.cs
@@ -1,27 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Book_Downloader
 {
-#warning Implement Logger
     class DefaultLogger : ILogger
     {
         public void Error(Severity severity, string message, params object[] elements)
         {
-            throw new NotImplementedException();
+            WriteMessage("ERROR",severity,message,elements);
         }
 
         public void Signal(Severity severity, string message, params object[] elements)
         {
-            throw new NotImplementedException();
+            WriteMessage("SIGNAL", severity, message, elements);
         }
 
         public void Warning(Severity severity, string message, params object[] elements)
         {
-            throw new NotImplementedException();
+            WriteMessage("WARNING", severity, message, elements);
+        }
+
+        private void WriteMessage(string type,Severity severity,string message,params object[] elements)
+        {
+            using (StreamWriter writer = new StreamWriter($"{type}s.txt", true))
+            {
+                writer.Write($"{severity} {type} {message} :");
+                foreach (object element in elements) { writer.Write($"{element}\t"); }
+                writer.WriteLine();
+            }
         }
     }
 }
